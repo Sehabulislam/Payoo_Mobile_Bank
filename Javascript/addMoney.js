daa83af6122042da386b5cc8fda8b6c198d6862c
Sehabulislam <shehabulislam244@gmail.com>
2025-08-24 03:02:33 +0600

    added the cashout section

--- Javascript/addMoney.js
+++ Javascript/addMoney.js
@@ -1,5 +1,5 @@
+// Add Money Feature 
 const validPin = 2211;
-
 document.getElementById('add-Money-btn').addEventListener('click', function(e){
     e.preventDefault()
     const bank = document.getElementById('bank').value
@@ -8,7 +8,6 @@ document.getElementById('add-Money-btn').addEventListener('click', function(e){
     const pinNumber = parseInt(document.getElementById('pin-number').value)
 
     const totalAmount = parseInt(document.getElementById('totalAmount').innerText)
-
     if(accountNumber.length < 11 ){
         alert('Please Fill Up The Account Number To 11 Digit.')
         return;
@@ -17,8 +16,38 @@ document.getElementById('add-Money-btn').addEventListener('click', function(e){
         alert('Please Fill Up The Pin Number To 4 Digit.')
         return;
     }
-
     const totalNewAmount = addAmount + totalAmount;
     document.getElementById('totalAmount').innerText = totalNewAmount
-   
+})
+// Cash Out Feature 
+document.getElementById('cashOut-btn').addEventListener('click',function(e){
+    e.preventDefault()
+    const agentNumber = document.getElementById('agent-number').value;
+    const addAmountCash = parseInt(document.getElementById('add-amount-cash').value);
+    const pinNumber = parseInt(document.getElementById('pin-number-cash').value);
+
+    const totalAmount = parseInt(document.getElementById('totalAmount').innerText)
+    if(agentNumber.length < 11 ){
+        alert('Please Fill Up The Agent Number To 11 Digit.')
+        return;
+    }
+    else if(pinNumber !== validPin ){
+        alert('Please Fill Up The Pin Number To 4 Digit.')
+        return;
+    }
+    const totalNewAmount = totalAmount - addAmountCash ;
+    document.getElementById('totalAmount').innerText = totalNewAmount;
+})
+
+// toggling feature 
+document.getElementById('addMoney').addEventListener('click',function(){
+    document.getElementById('cashOut-money').style.display = 'none';
+    document.getElementById('add-money').style.display = 'block';
+
+})
+
+document.getElementById('cashOut').addEventListener('click',function(){
+    document.getElementById('add-money').style.display = 'none';
+    document.getElementById('cashOut-money').style.display = 'block';
+
 })
