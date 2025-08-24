d9297a0afc47ea2cb9b9edb037b25a5c54482198
Sehabulislam <shehabulislam244@gmail.com>
2025-08-25 02:04:35 +0600

    added the get bonus section and pay bill section

--- Javascript/addMoney.js
+++ Javascript/addMoney.js
@@ -1,19 +1,50 @@
-// Add Money Feature 
+//function to get input values
+function getInputValueNumber(id){
+    const inputField = document.getElementById(id);
+    const inputFieldValue = inputField.value;
+    const inputFieldValueNumber = parseInt(inputFieldValue);
+
+    return inputFieldValueNumber;
+}
+
+function  getInputValue(id){
+    const inputField = document.getElementById(id);
+    const inputFieldValue = inputField.value;
+    return inputFieldValue;
+}
+//function to get innerText
+function getInnerText(id){
+    const element = document.getElementById(id);
+    const elementValue = element.innerText;
+    const elementValueNumber = parseInt(elementValue)
+    return elementValueNumber;
+}
 const validPin = 2211;
+//function to toggle
+function handleToggle(id){
+   const forms = document.getElementsByClassName('form');
+     for(const form of forms){
+    form.style.display = 'none';
+   }
+   document.getElementById(id).style.display = 'block'
+}
+
+// Add Money Feature 
 document.getElementById('add-Money-btn').addEventListener('click', function(e){
     e.preventDefault()
-    const bank = document.getElementById('bank').value
-    const accountNumber = document.getElementById('account-number').value
-    const addAmount = parseInt(document.getElementById('add-amount').value)
-    const pinNumber = parseInt(document.getElementById('pin-number').value)
+    const bank = getInputValue('bank');
+    const accountNumber = getInputValue('account-number')
+    const addAmount = getInputValueNumber('add-amount')
+    const pinNumber = getInputValueNumber('pin-number');
 
-    const totalAmount = parseInt(document.getElementById('totalAmount').innerText)
-    if(accountNumber.length < 11 ){
+    const totalAmount = getInnerText('totalAmount')
+    //account-number and pin check
+    if(accountNumber.length !== 11){
         alert('Please Fill Up The Account Number To 11 Digit.')
         return;
     }
     else if(pinNumber !== validPin ){
-        alert('Please Fill Up The Pin Number To 4 Digit.')
+        alert('Invalid Pin Number. Please Try Again')
         return;
     }
     const totalNewAmount = addAmount + totalAmount;
@@ -22,32 +53,68 @@ document.getElementById('add-Money-btn').addEventListener('click', function(e){
 // Cash Out Feature 
 document.getElementById('cashOut-btn').addEventListener('click',function(e){
     e.preventDefault()
-    const agentNumber = document.getElementById('agent-number').value;
-    const addAmountCash = parseInt(document.getElementById('add-amount-cash').value);
-    const pinNumber = parseInt(document.getElementById('pin-number-cash').value);
+    const agentNumber = getInputValue('agent-number');
+    const addAmountCash = getInputValueNumber('add-amount-cash');
+    const pinNumber = getInputValueNumber('pin-number-cash');
 
-    const totalAmount = parseInt(document.getElementById('totalAmount').innerText)
-    if(agentNumber.length < 11 ){
+    const totalAmount = getInnerText('totalAmount');
+    // mobile-number and pin check
+    if(agentNumber.length !== 11 ){
         alert('Please Fill Up The Agent Number To 11 Digit.')
         return;
     }
     else if(pinNumber !== validPin ){
-        alert('Please Fill Up The Pin Number To 4 Digit.')
+        alert('Invalid Pin Number. Please Try Again')
         return;
     }
     const totalNewAmount = totalAmount - addAmountCash ;
     document.getElementById('totalAmount').innerText = totalNewAmount;
 })
+//transfer money
+document.getElementById("send-btn").addEventListener('click', function(e){
+    e.preventDefault()
+    const userNumber = getInputValue("user-number");
+    const transferAmount = getInputValueNumber("transfer-amount");
+    const pinNumber = getInputValueNumber("pinNumber");
+    
+    const totalAmount = getInnerText('totalAmount');
+    //user-account-number and pin check
+    if(userNumber.length !== 11 ){
+        alert('Please Fill Up The User Number To 11 Digit.')
+        return;
+    }
+    else if(pinNumber !== validPin ){
+        alert('Invalid Pin Number. Please Try Again')
+        return;
+    }
+    const totalNewAmount = totalAmount - transferAmount ;
+     document.getElementById('totalAmount').innerText = totalNewAmount;
+})
 
 // toggling feature 
+
+// addMoney 
 document.getElementById('addMoney').addEventListener('click',function(){
-    document.getElementById('cashOut-money').style.display = 'none';
-    document.getElementById('add-money').style.display = 'block';
-
+        handleToggle("add-money")
 })
 
+//cashOut 
 document.getElementById('cashOut').addEventListener('click',function(){
-    document.getElementById('add-money').style.display = 'none';
-    document.getElementById('cashOut-money').style.display = 'block';
+   handleToggle('cashOut-money')
+}) 
 
+//transferMoney
+document.getElementById('transferMoney').addEventListener('click',function(){
+   handleToggle('transfer-money')
 })
+
+// get bonus
+document.getElementById('get-your-bonus').addEventListener('click',function(){
+    handleToggle('bonus-money')
+})
+
+// payMoney 
+document.getElementById('pay-money').addEventListener('click',function(){
+   handleToggle('pay-bill')
+})
+
--- Javascript/script.js
+++ Javascript/script.js
@@ -1,20 +1,21 @@
 //login button functionality
 document.getElementById('loginButton').addEventListener('click', function(e){
     e.preventDefault()
-    const mobileNumber = 12345678910;
-    const pinNumber = 2211;
-    const mobileNumberValue = document.getElementById('mobile-number').value
-    const mobileNumberValueConverted = parseInt(mobileNumberValue);
+  
+const mobileNumber = 12345678910;
+const pinNumber = 2211;
+const mobileNumberValue = document.getElementById('mobile-number').value
+const mobileNumberValueConverted = parseInt(mobileNumberValue);
 
-    const pinNumberValue = document.getElementById('pin-number').value
-    const pinNumberValueConverted = parseInt(pinNumberValue);
+const pinNumberValue = document.getElementById('pin-number').value
+const pinNumberValueConverted = parseInt(pinNumberValue);
 
-    // console.log(mobileNumberValueConverted,pinNumberValueConverted)
+// console.log(mobileNumberValueConverted,pinNumberValueConverted)
 
-    if(mobileNumberValueConverted === mobileNumber && pinNumberValueConverted === pinNumber){
-       window.location.href="home.html"
-    }
-    else{
-        alert('Not Matched. Pls try again.')
+if(mobileNumberValueConverted === mobileNumber && pinNumberValueConverted === pinNumber){
+   window.location.href="home.html"
+}
+else{
+        alert('Provide Valid Information. ')
     }
 })
